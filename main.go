5f6750a0e4d14c7d3373ae714d341898f5806256
Jonathan Innis <joinnis@amazon.com>
2025-06-25 11:49:43 -0700

    Fix misses in reconciliation logic

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/csv"
 	"fmt"
+	"log"
 	"os"
 	"sync"
 	"time"
@@ -11,11 +12,12 @@ import (
 	"github.com/samber/lo"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 	controllerruntime "sigs.k8s.io/controller-runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	v1 "sigs.k8s.io/karpenter/pkg/apis/v1"
@@ -29,7 +31,7 @@ var unhealthyCondition = corev1.NodeCondition{
 var tolerationDuration = 30 * time.Second
 
 func main() {
-	log.SetLogger(logging.NopLogger)
+	ctrllog.SetLogger(logging.NopLogger)
 	ctx := context.Background()
 	config := ctrl.GetConfigOrDie()
 	config.QPS = 5000
@@ -53,6 +55,7 @@ type nodeWatcher struct {
 	unhealthyNodeTime   *sync.Map
 	startDeleteNodeTime *sync.Map
 	endDeleteNodeTime   *sync.Map
+	once                sync.Once
 }
 
 func (*nodeWatcher) Name() string {
@@ -60,31 +63,38 @@ func (*nodeWatcher) Name() string {
 }
 
 func (c *nodeWatcher) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
+	c.once.Do(func() {
+		log.Printf("Started Node Watcher...")
+	})
 	node := &corev1.Node{}
 	if err := c.kubeClient.Get(ctx, request.NamespacedName, node); err != nil {
 		if errors.IsNotFound(err) {
 			// If we haven't previously seen the node getting deleted, then we use the current time as the startDeleteTime
-			startDeleteTime, loaded := c.startDeleteNodeTime.LoadOrStore(request.NamespacedName, time.Now())
-			unhealthyTime, ok := c.unhealthyNodeTime.Load(client.ObjectKeyFromObject(node))
+			startDeleteTime, loaded := c.startDeleteNodeTime.LoadOrStore(request.NamespacedName.String(), time.Now())
+			unhealthyTime, ok := c.unhealthyNodeTime.Load(request.NamespacedName.String())
 			// If we haven't previously seen the node getting deleted, then we need to log the StartNodeDelete event
-			if ok && !loaded {
-				c.writer.WriteAll([][]string{{"StartNodeDelete", node.Name, fmt.Sprint((startDeleteTime.(time.Time).Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
-
+			if ok {
+				if !loaded {
+					c.writer.WriteAll([][]string{{"StartNodeDelete", node.Name, fmt.Sprint((startDeleteTime.(time.Time).Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
+				}
+				c.writer.WriteAll([][]string{{"EndNodeDelete", node.Name, fmt.Sprint((time.Since(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
 			}
 			c.endDeleteNodeTime.Store(request.NamespacedName, time.Now())
-			c.writer.WriteAll([][]string{{"EndNodeDelete", node.Name, fmt.Sprint((startDeleteTime.(time.Time).Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
 			return reconcile.Result{}, nil
 		}
 		return reconcile.Result{}, err
 	}
 	if cond := GetCondition(node, unhealthyCondition.Type); cond.Status == corev1.ConditionFalse {
-		c.unhealthyNodeTime.LoadOrStore(client.ObjectKeyFromObject(node), cond.LastTransitionTime)
+		c.unhealthyNodeTime.LoadOrStore(client.ObjectKeyFromObject(node).String(), cond.LastTransitionTime.Time)
 	}
 	if !node.DeletionTimestamp.IsZero() {
-		_, loaded := c.startDeleteNodeTime.LoadOrStore(client.ObjectKeyFromObject(node), node.DeletionTimestamp.Time)
-		unhealthyTime, ok := c.unhealthyNodeTime.Load(client.ObjectKeyFromObject(node))
-		if ok && !loaded {
-			c.writer.WriteAll([][]string{{"StartNodeDelete", node.Name, fmt.Sprint((node.DeletionTimestamp.Time.Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
+		unhealthyTime, ok := c.unhealthyNodeTime.Load(client.ObjectKeyFromObject(node).String())
+		_, ok = unhealthyTime.(time.Time)
+		if ok {
+			_, loaded := c.startDeleteNodeTime.LoadOrStore(client.ObjectKeyFromObject(node).String(), node.DeletionTimestamp.Time)
+			if !loaded {
+				c.writer.WriteAll([][]string{{"StartNodeDelete", node.Name, fmt.Sprint((node.DeletionTimestamp.Time.Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
+			}
 		}
 	}
 	return reconcile.Result{}, nil
@@ -94,7 +104,7 @@ func (c *nodeWatcher) SetupWithManager(mgr manager.Manager) error {
 	return controllerruntime.NewControllerManagedBy(mgr).
 		Named(c.Name()).
 		For(&corev1.Node{}).
-		WithOptions(controller.Options{MaxConcurrentReconciles: 1000}).
+		WithOptions(controller.Options{MaxConcurrentReconciles: 1000, CacheSyncTimeout: time.Minute * 5}).
 		Complete(c)
 }
 
@@ -107,6 +117,7 @@ type nodeClaimWatcher struct {
 	endDeleteNodeClaimTime   *sync.Map
 	instanceTerminatingTime  *sync.Map
 	nodeDrainCompletedTime   *sync.Map
+	once                     sync.Once
 }
 
 func (*nodeClaimWatcher) Name() string {
@@ -114,6 +125,9 @@ func (*nodeClaimWatcher) Name() string {
 }
 
 func (c *nodeClaimWatcher) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
+	c.once.Do(func() {
+		log.Printf("Started NodeClaim Watcher...")
+	})
 	nodeClaim := &v1.NodeClaim{}
 	if err := c.kubeClient.Get(ctx, request.NamespacedName, nodeClaim); err != nil {
 		if errors.IsNotFound(err) {
@@ -122,38 +136,46 @@ func (c *nodeClaimWatcher) Reconcile(ctx context.Context, request reconcile.Requ
 				return reconcile.Result{}, nil
 			}
 			// If we haven't previously seen the nodeclaim getting deleted, then we use the current time as the startDeleteTime
-			startDeleteTime, loaded := c.startDeleteNodeClaimTime.LoadOrStore(request.NamespacedName, time.Now())
-			unhealthyTime, ok := c.unhealthyNodeTime.Load(client.ObjectKeyFromObject(nodeClaim))
+			startDeleteTime, loaded := c.startDeleteNodeClaimTime.LoadOrStore(request.NamespacedName.String(), time.Now())
+			unhealthyTime, ok := c.unhealthyNodeTime.Load(types.NamespacedName{Name: nodeName.(string)}.String())
 			// If we haven't previously seen the node getting deleted, then we need to log the StartNodeClaimDelete event
-			if ok && !loaded {
-				c.writer.WriteAll([][]string{{"StartNodeClaimDelete", nodeName.(string), fmt.Sprint((startDeleteTime.(time.Time).Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
+			if ok {
+				if !loaded {
+					c.writer.WriteAll([][]string{{"StartNodeClaimDelete", nodeName.(string), fmt.Sprint((startDeleteTime.(time.Time).Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
+				}
+				c.writer.WriteAll([][]string{{"EndNodeClaimDelete", nodeName.(string), fmt.Sprint((time.Since(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
 			}
 			c.endDeleteNodeClaimTime.Store(request.NamespacedName, time.Now())
-			c.writer.WriteAll([][]string{{"EndNodeClaimDelete", nodeName.(string), fmt.Sprint((startDeleteTime.(time.Time).Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
 			return reconcile.Result{}, nil
 		}
 		return reconcile.Result{}, err
 	}
 	c.nodeClaimToNodeName.Store(request.NamespacedName, nodeClaim.Status.NodeName)
 	if !nodeClaim.DeletionTimestamp.IsZero() {
-		_, loaded := c.startDeleteNodeClaimTime.LoadOrStore(client.ObjectKeyFromObject(nodeClaim), nodeClaim.DeletionTimestamp.Time)
-		unhealthyTime, ok := c.unhealthyNodeTime.Load(client.ObjectKeyFromObject(nodeClaim))
-		if ok && !loaded {
-			c.writer.WriteAll([][]string{{"StartNodeClaimDelete", nodeClaim.Status.NodeName, fmt.Sprint((nodeClaim.DeletionTimestamp.Time.Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
+		unhealthyTime, ok := c.unhealthyNodeTime.Load(types.NamespacedName{Name: nodeClaim.Status.NodeName}.String())
+		if ok {
+			_, loaded := c.startDeleteNodeClaimTime.LoadOrStore(client.ObjectKeyFromObject(nodeClaim).String(), nodeClaim.DeletionTimestamp.Time)
+			if !loaded {
+				c.writer.WriteAll([][]string{{"StartNodeClaimDelete", nodeClaim.Status.NodeName, fmt.Sprint((nodeClaim.DeletionTimestamp.Time.Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
+			}
 		}
 	}
 	if cond := nodeClaim.StatusConditions().Get(v1.ConditionTypeInstanceTerminating); cond.IsTrue() {
-		_, loaded := c.instanceTerminatingTime.LoadOrStore(request.NamespacedName, cond.LastTransitionTime.Time)
-		unhealthyTime, ok := c.unhealthyNodeTime.Load(client.ObjectKeyFromObject(nodeClaim))
-		if ok && !loaded {
-			c.writer.WriteAll([][]string{{"StartInstanceTerminating", nodeClaim.Status.NodeName, fmt.Sprint((cond.LastTransitionTime.Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
+		unhealthyTime, ok := c.unhealthyNodeTime.Load(types.NamespacedName{Name: nodeClaim.Status.NodeName}.String())
+		if ok {
+			_, loaded := c.instanceTerminatingTime.LoadOrStore(request.NamespacedName, cond.LastTransitionTime.Time)
+			if !loaded {
+				c.writer.WriteAll([][]string{{"StartInstanceTerminating", nodeClaim.Status.NodeName, fmt.Sprint((cond.LastTransitionTime.Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
+			}
 		}
 	}
 	if cond := nodeClaim.StatusConditions().Get(v1.ConditionTypeDrained); cond.IsTrue() {
-		_, loaded := c.nodeDrainCompletedTime.LoadOrStore(request.NamespacedName, cond.LastTransitionTime.Time)
-		unhealthyTime, ok := c.unhealthyNodeTime.Load(client.ObjectKeyFromObject(nodeClaim))
-		if ok && !loaded {
-			c.writer.WriteAll([][]string{{"EndNodeDrain", nodeClaim.Status.NodeName, fmt.Sprint((cond.LastTransitionTime.Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
+		unhealthyTime, ok := c.unhealthyNodeTime.Load(types.NamespacedName{Name: nodeClaim.Status.NodeName}.String())
+		if ok {
+			_, loaded := c.nodeDrainCompletedTime.LoadOrStore(request.NamespacedName, cond.LastTransitionTime.Time)
+			if !loaded {
+				c.writer.WriteAll([][]string{{"EndNodeDrain", nodeClaim.Status.NodeName, fmt.Sprint((cond.LastTransitionTime.Sub(unhealthyTime.(time.Time)) - tolerationDuration).Milliseconds())}})
+			}
 		}
 	}
 	return reconcile.Result{}, nil
@@ -163,7 +185,7 @@ func (c *nodeClaimWatcher) SetupWithManager(mgr manager.Manager) error {
 	return controllerruntime.NewControllerManagedBy(mgr).
 		Named(c.Name()).
 		For(&v1.NodeClaim{}).
-		WithOptions(controller.Options{MaxConcurrentReconciles: 1000}).
+		WithOptions(controller.Options{MaxConcurrentReconciles: 1000, CacheSyncTimeout: time.Minute * 5}).
 		Complete(c)
 }
 
